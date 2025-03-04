e6513cddb443238af1c1ff66bce5f48cfa14f32d
ume <chi.nguyenthiyen.203@gmail.com>
2025-03-04 23:53:04 +0700

    Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
 fetch("../html/components/header.html")
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("header-container").innerHTML = data;
-        });
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header-container").innerHTML = data;
+  });
+
+fetch("../html/components/bottomNav.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("bottom-nav-container").innerHTML = data;
+  });
+
+window.addEventListener("scroll", () => {
+  const navMenuSections = document.querySelectorAll(".nav-menu-section");
+  const scrollY = window.pageYOffset;
+
+  navMenuSections.forEach((navMenuSection) => {
+    let sectionHeight = navMenuSection.offsetHeight;
+    let sectionTop = navMenuSection.offsetTop - 50;
+    let id = navMenuSection.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.add("current");
+    } else {
+      document.querySelector(".bottom-nav .menu li a[href*=" + id + "]").classList.remove("current");
+    }
+  });
+});
