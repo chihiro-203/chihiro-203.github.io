184332967829d83fdcc54b701d21d30c55f465fe
ume <chi.nguyenthiyen.203@gmail.com>
2025-03-13 02:23:08 +0700

    Commit scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,12 +29,63 @@ window.addEventListener("scroll", () => {
     let id = navMenuSection.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      let activeLink = document.querySelector(
-        `.bottom-nav .menu li a[href="#${id}"]`
-      );
-      if (activeLink) {
-        activeLink.classList.add("current");
-      }
+      document
+        .querySelector(".bottom-nav .menu li a[href*=" + id + "]")
+        .classList.add("current");
+    } else {
+      document
+        .querySelector(".bottom-nav .menu li a[href*=" + id + "]")
+        .classList.remove("current");
     }
   });
 });
+
+// bottom navigation appears when scrolling
+// window.addEventListener("DOMContentLoaded", () => {
+//   const bottomNav = document.querySelector(".bottom-nav");
+//   bottomNav.classList.toggle("active", window.scrollY < 10);
+// });
+
+// const bottomNav = document.querySelector(".bottom-nav");
+// const menuHideBtn = document.querySelector(".menu-hide-btn");
+// const menuShowBtn = document.querySelector(".menu-show-btn");
+// var navTimeout;
+// console.log(!bottomNav)
+// show/hide bottom navigation through scrolling
+// window.addEventListener("scroll", () => {
+
+//   const bottomNav = document.querySelector(".bottom-nav");
+//   const menuHideBtn = document.querySelector(".menu-hide-btn");
+//   const menuShowBtn = document.querySelector(".menu-show-btn");
+//   var navTimeout;
+
+//   bottomNav.classList.add("active");
+//   menuShowBtn.classList.remove("active");
+
+//   if (window.scrollY < 10) {
+//     menuHideBtn.classList.remove("active");
+//     menuShowBtn.classList.remove("active");
+
+//     function scrollStopped() {
+//       bottomNav.classList.add("active");
+//     }
+//     clearTimeout(navTimeout);
+//     navTimeout = setTimeout(scrollStopped, 2500);
+//   }
+
+//   if (window.scrollY > 10) {
+//     menuHideBtn.classList.add("active");
+//     function scrollStopped() {
+//       bottomNav.classList.remove("active");
+//       menuShowBtn.classList.add("active");
+//     }
+//     clearTimeout(navTimeout);
+//     navTimeout = setTimeout(scrollStopped, 2500);
+//   }
+// });
+
+// document.querySelector(".menu-hide-btn").addEventListener("click", () => {
+//   document.querySelector(".bottom-nav").toggle("active");
+//   document.querySelector(".menu-hide-btn").toggle("active");
+//   document.querySelector(".menu-show-btn").toggle("active");
+// })
